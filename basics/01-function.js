11195a0f490eee428555263c8ffe398f3c222628
Bharat-Gaur <105897826+Bharat-Gaur@users.noreply.github.com>
2023-12-02 17:19:10 +0000

    changes

--- basics/01-function.js
+++ basics/01-function.js
@@ -6,3 +6,29 @@ function sayMyName(){
 }
 
 sayMyName();
+
+function addTwoNum(num1, num2){
+    return num1 + num2;
+}
+
+const result = addTwoNum(5, 6);
+console.log(result);
+
+// function loginUser(username){
+//     console.log(`${username} just has loggedin`);
+//     return;
+// }
+//function loginUser("hitesh");
+
+// function loginUser(username){
+//     return `${username} just has loggedin2`
+// }
+// console.log(loginUser("ramesh"));
+
+function loginUser(username){
+    if(!username){
+        console.log("please enter the Username")
+        return;
+    }
+    console.log(loginUser());
+}
